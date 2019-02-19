e6a54dfee4d668cf25a2589151e8ed88fbff682d
AAgudo27 <47173756+AAgudo27@users.noreply.github.com>
2019-02-19 14:15:31 +0100

    Update conexion.php

--- musica/db/conexion.php
+++ musica/db/conexion.php
@@ -1,5 +1,5 @@
 <?php
-   define('DB_SERVER', 'localhost');
+   define('DB_SERVER', '10.129.37.111');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'rootroot');
    define('DB_DATABASE', 'musica');
