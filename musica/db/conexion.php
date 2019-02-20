70d1d130214aebb990cfdd9d9a3480b39c5a5e9d
AAgudo27 <47173756+AAgudo27@users.noreply.github.com>
2019-02-20 08:59:18 +0100

    Update conexion.php

--- musica/db/conexion.php
+++ musica/db/conexion.php
@@ -1,8 +1,8 @@
 <?php
-   define('DB_SERVER', '10.129.37.111');
+   define('DB_SERVER', '10.131.73.18');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'rootroot');
-   define('DB_DATABASE', 'musica');
+   define('DB_DATABASE', 'spotify');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    
    if (!$db) {
